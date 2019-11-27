eb892328fbdee8d86fee7e0b073af694bf2590ec
Gizer <Gizer@users.noreply.github.com>
2019-11-27 21:17:57 +0100

    Add files via upload

---
+++ IO/IO/DBConnector.cs
@@ -0,0 +1,87 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace IO
+{
+    public static class DBConnector
+    {
+        private static OracleConnection connection = null;
+        private static string encString = StringCipher.RandomString();
+        private static string Login;
+        private static string Pass;
+
+        public static void SetLogin(string login)
+        {
+            Login = StringCipher.Encrypt(login, encString);
+        }
+        public static void SetPass(string pass)
+        {
+            Pass = StringCipher.Encrypt(pass, encString);
+        }
+        public static bool Open()
+        {
+            if (connection == null)
+                connection = new OracleConnection("User Id=" + StringCipher.Decrypt(Login,encString) + ";Password=" + StringCipher.Decrypt(Pass, encString) + "; Data Source=*Integrated Security=no;");
+            else if (connection.State == System.Data.ConnectionState.Open)
+                return true;
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            if (connection.State == System.Data.ConnectionState.Open)
+                return true;
+            return false;
+        }
+
+        public static void Close()
+        {
+            if (connection == null)
+                return;
+            else if (connection.State == System.Data.ConnectionState.Closed)
+                return;
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static OracleDataReader Query(string query)
+        {
+            if (String.IsNullOrEmpty(query))
+                return null;
+            if (connection == null || connection.State == System.Data.ConnectionState.Closed)
+                try
+                {
+                    if (!Open())
+                        return null;
+                }
+                catch
+                {
+                    return null;
+                }
+            try
+            {
+                OracleDataReader dr = new OracleCommand(query, connection).ExecuteReader();
+                Close();
+                return dr;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ IO/IO/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace IO
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPassenger_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            DBConnector.SetLogin(textBoxLogin.Text);
+            DBConnector.SetPass(textBoxPass.Text);
+            if(!DBConnector.Open())
+            {
+                MessageBox.Show("Niepoprawne dane");
+            }
+            else
+            {
+                //jakaś query żeby ustalić czy to pracownik czy kierownik
+                var result = DBConnector.Query(String.Format("SELECT * FROM {0}_ROLE_PRIVS", textBoxLogin.Text));
+                //odpowiednio sprawdzić result
+                if(true)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void textBoxTextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBoxLogin.Text) && !String.IsNullOrEmpty(textBoxPass.Text))
+                buttonLogin.Enabled = true;
+            else
+                buttonLogin.Enabled = false;
+        }
+    }
+}
